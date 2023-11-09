9054b4389028275d02cf585637653be2aade7997
Your Name <alu0101642468@ull.edu.es>
2023-11-09 21:51:26 +0000

    Cmake

---
+++ build/CMakeFiles/MiEjecutable.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MiEjecutable.dir/proyecto/src/hola.cc.o"
+  "MiEjecutable"
+  "MiEjecutable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MiEjecutable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
