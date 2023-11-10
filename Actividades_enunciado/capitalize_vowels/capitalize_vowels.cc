b071865fa58935fb1e13b0088df474d2dbc604a2
Your Name <alu0101642468@ull.edu.es>
2023-11-10 18:54:09 +0000

    gitignore

---
+++ Actividades_enunciado/capitalize_vowels/capitalize_vowels.cc
@@ -0,0 +1,23 @@
+ /**
+ * Universidad de la Laguna
+ * Escuela superior de ingeniería y tecnología
+ * Grado en ingeniería informática
+ * Informática Básica
+ *
+ * @date Oct 10 2023
+ * @autor Saúl Lorenzo Armas
+ * @breef This program take as a parameter a string of characters without spaces
+ *        and modify it so that the consonants are lowercase and the vowels are uppercase.
+ *        
+ * @bugs There aren't problems in the program
+ * @see https://github.com/IB-2023-2024/P09-functions2/blob/main/functions.md
+ */
+
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
---
+++ Actividades_enunciado/hypotenuse/hypotenuse.cc
@@ -0,0 +1,25 @@
+ /**
+ * Universidad de la Laguna
+ * Escuela superior de ingeniería y tecnología
+ * Grado en ingeniería informática
+ * Informática Básica
+ *
+ * @date Oct 10 2023
+ * @autor Saúl Lorenzo Armas
+ * @breef This program 
+ * @bugs There aren't problems in the program
+ * @see https://github.com/IB-2023-2024/P09-functions2/blob/main/functions.md
+ */
+
+
+#include <iostream>
+
+
+
+
+
+
+int main() {
+    return 0;
+}
+
