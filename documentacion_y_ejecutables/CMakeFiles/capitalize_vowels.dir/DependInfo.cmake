2fa6c1a56407eed0720aea013b1a9f3e852efdd7
Your Name <alu0101642468@ull.edu.es>
2023-11-12 19:27:41 +0000

    documentacion

---
+++ documentacion_y_ejecutables/CMakeFiles/capitalize_vowels.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/usuario/Practica_09_Funciones.2/Actividades_enunciado/capitalize_vowels/capitalize_vowels.cc" "/home/usuario/Practica_09_Funciones.2/documentacion_y_ejecutables/CMakeFiles/capitalize_vowels.dir/Actividades_enunciado/capitalize_vowels/capitalize_vowels.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
